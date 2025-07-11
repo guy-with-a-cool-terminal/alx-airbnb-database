efc1316a70743b5df9576b21479f1c0959b1be36
guy-with-a-cool-terminal <njugunabriian@gmail.com>
2025-07-11 18:33:13 +0300

    rankrownumber

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -12,12 +12,12 @@ GROUP BY
 ORDER BY
     total_bookings DESC;
 
--- Use ROW_NUMBER() to rank properties based on total number of bookings
+-- Use ROW_NUMBER() to rank properties by total bookings
 SELECT
     property_id,
     property_name,
     total_bookings,
-    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS row_number_rank
 FROM (
     SELECT
         p.id AS property_id,
@@ -29,6 +29,27 @@ FROM (
         bookings b ON p.id = b.property_id
     GROUP BY
         p.id, p.name
-) AS ranked_properties
+) AS subquery_row_number
 ORDER BY
-    booking_rank;
+    row_number_rank;
+
+-- Use RANK() to rank properties by total bookings
+SELECT
+    property_id,
+    property_name,
+    total_bookings,
+    RANK() OVER (ORDER BY total_bookings DESC) AS rank_position
+FROM (
+    SELECT
+        p.id AS property_id,
+        p.name AS property_name,
+        COUNT(b.id) AS total_bookings
+    FROM
+        properties p
+    LEFT JOIN
+        bookings b ON p.id = b.property_id
+    GROUP BY
+        p.id, p.name
+) AS subquery_rank
+ORDER BY
+    rank_position;
