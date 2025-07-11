38df27597f63606a297f49c60fd87b5b0a31da07
guy-with-a-cool-terminal <njugunabriian@gmail.com>
2025-07-11 18:25:36 +0300

    Automated commit - 2025-07-11 18:25:36

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -10,7 +10,9 @@ SELECT
 FROM
     bookings b
 INNER JOIN
-    users u ON b.user_id = u.id;
+    users u ON b.user_id = u.id
+ORDER BY
+    b.id;
 
 -- LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
 SELECT
@@ -22,7 +24,9 @@ SELECT
 FROM
     properties p
 LEFT JOIN
-    reviews r ON p.id = r.property_id;
+    reviews r ON p.id = r.property_id
+ORDER BY
+    p.id;
 
 -- FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
 SELECT
@@ -34,5 +38,6 @@ SELECT
 FROM
     users u
 FULL OUTER JOIN
-    bookings b ON u.id = b.user_id;
-
+    bookings b ON u.id = b.user_id
+ORDER BY
+    u.id NULLS LAST, b.id NULLS LAST;
